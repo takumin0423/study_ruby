f56ddb15ce9c5af32bbd4a4896fac8dbd9a2a386
takumin0423 <takumin0423baske@gmail.com>
2021-01-12 22:40:26 +0900

    commit

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -36,7 +36,7 @@ class TestController < ApplicationController
 
       # 演算開始
       total_price = Order::Billing::Price.new(20000)
-      discount_price = Order::Billing::Price.new(20000 * 0.13)
+      discount_price = Order::Billing::Price.new(20000 * 0.1)
       shipping_fee = Order::Billing::Price.new(300)
 
       discounted_price = Order::Billing::Minus.new(total_price, discount_price).execute
@@ -55,73 +55,74 @@ class TestController < ApplicationController
       render json: { order: order, ordered_product: ordered_product, order_bill: order_bill }, status: 200
     end
   end
-
-  def order_builder
-    @order = Order.eager_load(:products, account: [:addresses, :account_rank]).all.last
-    order_builder = OrderBuilder.new(@order)
-    order_builder.set_mask.visible_account.visible_address.visible_order_details
-    render json: { order: order_builder.to_json }, status: 200
-  end
-
-  def order_builder_collection
-    @orders = Order.eager_load(:products, account: [:addresses, :account_rank]).limit(3)
-    order_builder_collection = OrderBuilderCollection.new(@orders)
-    order_builder_collection.set_mask
-    order_builder_collection.visible_account
-    collection_result = [].tap do |result|
-      order_builder_collection.each do |order_builder|
-        result << order_builder.to_json
-      end
-    end
-    render json: { order: collection_result }, status: 200
-  end
-
-  def order_query
-    shopper = Account.where(account_type: Account.account_types[:shopper]).last
-    product = Product.all.last
-    query = OrderQuery::WithAccount.new.by_account(shopper)
-    query = OrderQuery::WithProduct.new(query.relation).by_product(product).order_by(:name)
-    query = query.relation.limit(3)
-    render json: { query: query }, status: 200
-  end
-
-  def send_mail_plain
-    # 注文情報を1件取得
-    shopper = Account.where(account_type: Account.account_types[:shopper]).last
-    query = OrderQuery::WithAccount.new.by_account(shopper)
-    @order = query.relation.last
-
-    # OrderBuilderの生成
-    order_builder = OrderBuilder.new(@order)
-    order_builder.set_mask.visible_account.visible_address.visible_order_details
-
-    # メール本文の生成
-    text_report = ReportMail::TextReport.new(order_builder)
-
-    render plain: text_report.output_report, status: 200
-  end
-
-  def send_mail_html
-    render json: {}, status: 200
-  end
-
-  def formatter_plain
-    shopper = Account.where(account_type: Account.account_types[:shopper]).last
-    query = OrderQuery::WithAccount.new.by_account(shopper)
-    @order = query.relation.last
-
-    # OrderBuilderの生成
-    order_builder = OrderBuilder.new(@order)
-    order_builder = order_builder.set_mask.visible_account.visible_address.visible_order_details
-
-    # 通知本文の生成
-    notify = ReportChat::Notify.new(order_builder, ReportChat::TextFormatter.new)
-    render plain: notify.output, status: 200
-  end
-
-  private
-
-  def only_development
-    raise StandardError unless Rails.env == 'development'
-  end
 end
+
+def order_builder
+  @order = Order.eager_load(:products, account: [:addresses, :account_rank]).all.last
+  order_builder = OrderBuilder.new(@order)
+  order_builder.set_mask.visible_account.visible_address.visible_order_details
+  render json: { order: order_builder.to_json }, status: 200
+end
+
+def order_builder_collection
+  @orders = Order.eager_load(:products, account: [:addresses, :account_rank]).limit(3)
+  order_builder_collection = OrderBuilderCollection.new(@orders)
+  order_builder_collection.set_mask
+  order_builder_collection.visible_account
+  collection_result = [].tap do |result|
+    order_builder_collection.each do |order_builder|
+      result << order_builder.to_json
+    end
+  end
+  render json: { order: collection_result }, status: 200
+end
+
+def order_query
+  shopper = Account.where(account_type: Account.account_types[:shopper]).last
+  product = Product.all.last
+  query = OrderQuery::WithAccount.new.by_account(shopper)
+  query = OrderQuery::WithProduct.new(query.relation).by_product(product).order_by(:name)
+  query = query.relation.limit(3)
+  render json: { query: query }, status: 200
+end
+
+def send_mail_plain
+  # 注文情報を1件取得
+  shopper = Account.where(account_type: Account.account_types[:shopper]).last
+  query = OrderQuery::WithAccount.new.by_account(shopper)
+  @order = query.relation.last
+
+  # OrderBuilderの生成
+  order_builder = OrderBuilder.new(@order)
+  order_builder.set_mask.visible_account.visible_address.visible_order_details
+
+  # メール本文の生成
+  text_report = ReportMail::TextReport.new(order_builder)
+
+  render plain: text_report.output_report, status: 200
+end
+
+def send_mail_html
+  render json: {}, status: 200
+end
+
+def formatter_plain
+  shopper = Account.where(account_type: Account.account_types[:shopper]).last
+  query = OrderQuery::WithAccount.new.by_account(shopper)
+  @order = query.relation.last
+
+  # OrderBuilderの生成
+  order_builder = OrderBuilder.new(@order)
+  order_builder = order_builder.set_mask.visible_account.visible_address.visible_order_details
+
+  # 通知本文の生成
+  notify = ReportChat::Notify.new(order_builder, ReportChat::TextFormatter.new)
+  render plain: notify.output, status: 200
+end
+
+private
+
+def only_development
+  raise StandardError unless Rails.env == 'development'
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+a1 = AccountFactory.create('system')
+a1.attributes =	({
+  name: "System User",
+  password: "system"
+})
+a1.save
+
+a2 = AccountFactory.create('admin')
+a2.attributes =	({
+  name: "Test Admin",
+  password: "admin",
+  email: "test@com",
+  tel: "000999990000"
+})
+a2.save
+
+a3 = AccountFactory.create('shopper')
+a3.attributes =	({
+  name: "Shop Admin",
+  password: "shop",
+  email: "test@com",
+  tel: "000999990000"
+})
+a3.save
+
+address_new = Address.create(
+  account: a3,
+  postal_code: "1000000",
+  street: "Test Street",
+  city: "Test City",
+  state: "Test State",
+  country: "TestCounty",
+  )
+
+# Create Operation For Logging Function
+Operation.create({
+                   sub_type:1,
+                   name: "admin dashboard",
+                 })
+
+# Create Products and other related information
+product = Product.create({
+                           name: "テスト商品",
+                           description: "テスト商品の説明",
+                           ec_stock_id: 1,
+                           store_stock_id: 3,
+                           price: 1000
+                         })
+
+stock = Stock.create({
+                       product_id: product.id,
+                       ec_stock_amount: 3,
+                       store_stock_amount: 1
+                     })
+
+product2 = Product.create({
+                            name: "テスト商品2",
+                            description: "テスト商品の説明2",
+                            ec_stock_id: 1,
+                            store_stock_id: 3,
+                            price: 1200
+                          })
+
+stock2 = Stock.create({
+                        product_id: product2.id,
+                        ec_stock_amount: 3,
+                        store_stock_amount: 1
+                      })
+
+# Execute after 9章
+# UpdateStockInformationJob.new.perform
